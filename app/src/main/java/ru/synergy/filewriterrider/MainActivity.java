fc5c6542954711a8dbb0f90be52ae2c6bc72253e
fash25 <fash25rus@gmail.com>
2022-10-17 16:50:46 +1000

    Old shit

--- app/src/main/java/ru/synergy/filewriterrider/MainActivity.java
+++ app/src/main/java/ru/synergy/filewriterrider/MainActivity.java
@@ -3,9 +3,21 @@ package ru.synergy.filewriterrider;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 
 public class MainActivity extends AppCompatActivity {
+    private final static String FILE_NAME = "content.txt";
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -13,7 +25,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private File getExternalFilePath(){
+        return new File(getExternalFilesDir(null), FILE_NAME);
 
 
+    }
 
+          // Сохранение файла
+    public void saveText (View view) {
+        FileOutputStream fos = null;
+
+        try {
+            EditText textBox = (EditText) findViewById(R.id.editor);
+            String text = textBox.getText().toString();
+
+            fos = new FileOutputStream(getExternalFilePath());//openFileOutput(FILE_NAME,MODE_PRIVATE);
+            fos.write(text.getBytes(StandardCharsets.UTF_8));
+            Toast.makeText(this,"Файл успешно сохранен",Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
+        } finally {
+
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, e.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    // Открытие файла
+    public void openText(View view){
+        FileInputStream fin = null;
+        TextView textView = (TextView) findViewById(androidx.core.R.id.text);
+
+        File file = getExternalFilePath();
+
+        try {
+            fin = new FileInputStream(file);//openFileInput(FILE_NAME);
+            byte[] bytes;
+            bytes = new byte[((FileInputStream) fin).available()];
+            ((FileInputStream) fin).read(bytes);
+            String text = new String(bytes);
+            textView.setText(text);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
+        } finally {
+
+            try {
+                if (fin != null){
+                    fin.close();
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+                Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
